319a61a1f4e4607f4db5ff1c27b186b0289200aa
Lily <matthew@nowaffles.com>
2022-08-09 21:32:58 -0700

    Change metaprogramming style

--- ext/baba_vm/object.hpp
+++ ext/baba_vm/object.hpp
@@ -3,19 +3,21 @@
 
 #include <string>
 
-typedef Value BasicObject *;
-
+/* A basic, barebones object which all objects inherit from */
 struct BasicObject
 {
+    /* Define a method on this class */
     void define_method(std::string name, Value (*fn)(...), int argc);
 };
 
-struct Object : public BasicObject
-{
-};
+typedef Value BasicObject *;
 
-struct Class : public Object
-{
-};
+/* To be defined in object.cpp */
+extern Value Object;
+extern Value Class;
+
+/* Class definition functions */
+Value define_class(std::string name, Value super);
+Value define_class_under(std::string name, Value super, Value under);
 
 #endif
